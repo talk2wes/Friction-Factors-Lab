0d5ac41bab245cfedcd91f8d7c5a79866c371226
Wesley Johanson <talk2wes@gmail.com>
2022-04-14 15:47:29 -0700

    good

--- 1_HW_ChE118.py
+++ 1_HW_ChE118.py
@@ -2,10 +2,10 @@
 import ChE
 import numpy as np
 
-print ("\n\n")
-
 plt = ChE.ChEplot()
 plt.loadCSV("che118_hw.csv", skip=11)
+
+#FIXING THE DATA_____________________________________
 print("what the original data looked like")
 plt.printData()
 #Gotta fix the data, it's scrambled from the copy/paste
@@ -23,14 +23,22 @@ while(i < len(plt.data)):
 	y.append(orig[i]); i += 1
 	x_err.append(orig[i]); i += 1
 	y_err.append(orig[i]); i += 1
+print("what the corrected data looks like")
+plt.printData()
+#FIXING THE DATA_____________________________________
 
-print("what the new data looks like")
+
+
+labels = ["Pressure [Bar]", "Signal [Volts]", "Pressure_xError [bar]",\
+		"Signal_yError [Volts]" ]
+# plotFxns = []
 newData = np.array([x, y, x_err, y_err])
 plt.setData(newData, vars = 1)
-plt.printData()
+plt.setDataLabel(labels)
 m , b = np.polyfit(x,y,1)
+plt.printMeans()
 
-x_mean = np.mean()
+# x_mean = np.mean()
 
 
 
